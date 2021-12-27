5924ce33a1b871937d39a588010b9ea961529689
Markus Hedenborn <markushedenborn@gmail.com>
2021-12-27 18:17:34 +0100

    favorite function added(localstorage)

--- components/CrimePost/index.js
+++ components/CrimePost/index.js
@@ -1,15 +1,50 @@
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faHeartBroken } from '@fortawesome/free-solid-svg-icons'
 import styles from './CrimePost.module.scss'
 
+function FavoriteButton(post) {
+  const [favorite, setFavorite] = useState(false)
+  const currentlyAFavorite = <FontAwesomeIcon icon={faHeart} />
+  const notCurrentlyAFavorite = <FontAwesomeIcon icon={faHeartBroken} />
+
+  const toggleFavorite = () => {
+    const obj = {
+      id: post.post.id,
+      image: post.post.image,
+      title: post.post.title_type,
+      location: post.post.title_location,
+      date: post.post.date_human,
+      teaser: post.post.content_teaser,
+      link: post.post.external_source_link,
+    }
+
+    setFavorite((favorite) => {
+      if (favorite == true) {
+        localStorage.removeItem(obj.id, JSON.stringify(obj))
+      }
+      if (favorite == false) {
+        localStorage.setItem(obj.id, JSON.stringify(obj))
+      }
+      return !favorite
+    })
+  }
+
+  return (
+    <button
+      className={`${styles.crimeFav}`}
+      onClick={() => toggleFavorite(post)}
+      key={post.id}
+    >
+      {favorite === true ? currentlyAFavorite : notCurrentlyAFavorite}
+    </button>
+  )
+}
+
 export default function CrimePost({ data }) {
-  // function parseShittyApiRawData(html) {
-  //   let doc = new DOMParser().parseFromString(html, 'text/html')
-  //   return doc.body.textContent || ''
-  // }
-
-  console.log(data.image)
-
   return (
     <div className={`col-md-6 d-flex`}>
       <div className={`${styles.crimePost}`}>
@@ -21,10 +56,15 @@ export default function CrimePost({ data }) {
               layout="fill"
               objectFit="cover"
             />
-          ) : (<span className={`${styles.noImage}`}>Ingen bild genererad.</span>)}
+          ) : (
+            <span className={`${styles.noImage}`}>Ingen bild genererad.</span>
+          )}
         </section>
         <section>
-          <h3>{data.title_type}</h3>
+          <div className={`${styles.postHeader}`}>
+            <h3>{data.title_type}</h3>
+            <FavoriteButton post={data} />
+          </div>
           <h4>{data.title_location}</h4>
           <small>{data.date_human}</small>
           <p>{data.content_teaser}</p>

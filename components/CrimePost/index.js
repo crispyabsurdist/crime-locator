ff90c02debfffc4dce378705f8024f38dec63dd7
Markus Hedenborn <markushedenborn@gmail.com>
2021-12-28 19:23:27 +0100

    favorite button added to own component

--- components/CrimePost/index.js
+++ components/CrimePost/index.js
@@ -1,52 +1,10 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-
+import FavoriteButton from '../FavoriteButton'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faHeart,
-  faHeartBroken,
-  faClock,
-} from '@fortawesome/free-solid-svg-icons'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
 import styles from './CrimePost.module.scss'
-
-function FavoriteButton(post) {
-  const [favorite, setFavorite] = useState(false)
-  const isFav = <FontAwesomeIcon icon={faHeart} />
-  const isNotFav = <FontAwesomeIcon icon={faHeartBroken} />
-
-  const toggleFavorite = () => {
-    const obj = {
-      id: post.post.id,
-      image: post.post.image,
-      title: post.post.title_type,
-      location: post.post.title_location,
-      date: post.post.date_human,
-      teaser: post.post.content_teaser,
-      link: post.post.external_source_link,
-    }
-
-    setFavorite((favorite) => {
-      if (favorite == true) {
-        localStorage.removeItem(obj.id, JSON.stringify(obj))
-      }
-      if (favorite == false) {
-        localStorage.setItem(obj.id, JSON.stringify(obj))
-      }
-      return !favorite
-    })
-  }
-
-  return (
-    <button
-      className={`${styles.crimeFav}`}
-      onClick={() => toggleFavorite(post)}
-      key={post.id}
-    >
-      {favorite === true ? isFav : isNotFav}
-    </button>
-  )
-}
 
 export default function CrimePost({ data }) {
   return (
---
+++ components/FavoriteButton/index.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faHeartBroken } from '@fortawesome/free-solid-svg-icons'
+import styles from './FavoriteButton.module.scss'
+
+export default function FavoriteButton(post) {
+  const [favorite, setFavorite] = useState(false)
+  const isFav = <FontAwesomeIcon icon={faHeart} />
+  const isNotFav = <FontAwesomeIcon icon={faHeartBroken} />
+
+  const toggleFavorite = () => {
+    const obj = {
+      id: post.post.id,
+      image: post.post.image,
+      title: post.post.title_type,
+      location: post.post.title_location,
+      date: post.post.date_human,
+      teaser: post.post.content_teaser,
+      link: post.post.external_source_link,
+    }
+
+    setFavorite((favorite) => {
+      if (favorite == true) {
+        localStorage.removeItem(obj.id, JSON.stringify(obj))
+      }
+      if (favorite == false) {
+        localStorage.setItem(obj.id, JSON.stringify(obj))
+      }
+      return !favorite
+    })
+  }
+
+  return (
+    <button
+      className={`${styles.crimeFav}`}
+      onClick={() => toggleFavorite(post)}
+      key={post.id}
+    >
+      {favorite === true ? isFav : isNotFav}
+    </button>
+  )
+}

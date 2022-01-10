4563d641bbf7fe4b6bc13736374201e17637d964
Markus Hedenborn <markushedenborn@gmail.com>
2022-01-10 06:26:00 +0100

    map click function added. still needs a lot of work

---
+++ components/MapComponent/index.js
@@ -0,0 +1,50 @@
+import React, {useState, useCallback} from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+
+export default function MapComponent() {
+  const containerStyle = {
+    width: '800px',
+    height: '400px',
+  }
+
+  const center = {
+    lat: 57.779474596918945,
+    lng: 14.161760229345505,
+  }
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'AIzaSyBAvC5AIT83wF2zlF4r3KzeTFEUT7DnjBI',
+  })
+
+  const [map, setMap] = useState(null)
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds()
+    map.fitBounds(bounds)
+    setMap(map)
+  }, [])
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={15}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+      onClick={(ev) => {
+        'latitide = ', ev.latLng.lat()
+        'longitude = ', ev.latLng.lng()
+      }}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import LocationQuery from '../components/LocationQuery'
 import IndexQuery from '../components/IndexQuery'
+import MapQuery from '../components/MapsQuery'
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
         <title>Crime Locator</title>
       </Head>
       <Header title="Crime Locator" />
-      <LocationQuery />
+      <MapQuery />
       <IndexQuery />
       <Footer />
     </>
